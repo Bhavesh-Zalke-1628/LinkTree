eee66a91cb6d6fab061f0ae4217e748c328cfbcc
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-11-16 20:44:07 +0530

    initial commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <section className=" bg-green-600">
+        sdfds
+      </section>
+
+    </main>
+  );
+}
